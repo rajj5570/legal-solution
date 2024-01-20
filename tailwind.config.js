efceedac7c12b41f26e92bb4b6ac9f92db0efd69
rajib5570 <rajib5570@gmail.com>
2024-01-21 03:45:44 +0600

    navbar added

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        fontFamily: {
+            'primary-text': ['EB Garamond'],
+            'secondary-text': ['Poppins'],
+        }
+
+
+
+
+        
+      },
+    },
+    plugins: [],
+  }
